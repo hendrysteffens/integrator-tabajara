301dbf031383642ae3b82719dc20fa60f9dac9a9
hendry.steffens <hendry.steffens@senior.com.br>
2020-10-01 18:06:47 -0300

    Fazendo umas paradas ainda

--- src/main/kotlin/integrator/generator/sdl/ExtractSdlData.kt
+++ src/main/kotlin/integrator/generator/sdl/ExtractSdlData.kt
@@ -1,8 +1,15 @@
 package integrator.generator.sdl;
 
 class ExtractSdlData {
+
+    enum class ValidationType {
+        STRING_SIZE
+    }
+    data class Field(val name: String, val type: String, val validations :Map<ValidationType,String>)
+
+
     companion object {
-        fun extractData(entityText: String): Pair<String?, HashMap<String, String>> {
+        fun extractData(entityText: String): Pair<String?, List<Field>> {
             var entityLine = entityText.split("\n")
                     .get(1).split(" ")
             var size = entityLine.size
@@ -13,18 +20,30 @@ class ExtractSdlData {
             println(match?.value)
             var entitySplitted = entityText.split("\n")
             val entityName = "(?<=entity\\s).*(?=\\s\\{)".toRegex().find(entitySplitted.get(1))?.value
-            var fields = HashMap<String, String>();
+            var fields = ArrayList<Field>();
             entitySplitted
                     .filterNot { s -> s.trim().startsWith("\"") || s.trim().startsWith("}") || s.contains("entity") }
                     .forEach {
                         val fielLine = it.split(":");
-                        fields.put(fielLine.get(0).trim(), fielLine.get(1).trim())
+                        fields.add(Field(fielLine.get(0).trim(), processFieldType(fielLine.get(1).trim())))
                     }
 
-            fields.forEach { t: String, u: String ->
-                println("Campo : " + t + " do tipo : " + u)
+            fields.forEach {
+                println("Campo : " + it.name + " do tipo : " + it.type)
             }
             return Pair(entityName, fields)
+        }
+
+        private fun processFieldType(fieldType: String): String {
+            if (fieldType.contains("string")){
+                return processFieldStringType(fieldType);
+            }
+            return fieldType;
+        }
+
+        private fun processFieldStringType(fieldType: String): String {
+            var match = "(?<=\\(\\s).*(?=\\s\\))".toRegex().find(fieldType)
+            match?.value
         }
 
     }
